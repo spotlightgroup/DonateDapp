3ca4068395f7f3dad645c957d1ff721e241c5343
AbdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2018-05-12 13:44:15 +0300

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,29 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'home',
+    component: HomeComponent
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+   path: 'login',
+   component: LoginComponent,
+   data: { title: 'Login' }
+ },
+ {
+  path: 'signup',
+  component: RegisterComponent,
+  data: { title: 'signup' }
+}
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +37,13 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true }
+  )
   ],
   providers: [],
   bootstrap: [AppComponent]
