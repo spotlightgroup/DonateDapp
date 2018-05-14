e35e1a6e1ee4a03ebe79d648d0f2e3a44d75a3f9
AbdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2018-05-14 11:20:07 +0300

    modefy config file

--- dist/DonateDapp/polyfills.js
+++ dist/DonateDapp/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/rbk-09/Desktop/DonateDapp/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/abdullatif/Desktop/DonateDapp/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
--- dist/DonateDapp/styles.js
+++ dist/DonateDapp/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/rbk-09/Desktop/DonateDapp/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/abdullatif/Desktop/DonateDapp/src/styles.css */"./src/styles.css");
 
 
 /***/ })
