19f57369ae83c8b2acab20bcbdd83f98b59f9b4d
samersalmeh <ssalmeh@gmail.com>
2018-05-18 16:18:06 +0300

    setup profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,27 +8,43 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-model = {
-  fullName: "",
-  phoneNumber1: 0,
-  phoneNumber2: 0,
-  address: "",
-  email: "",
-  Overview: ""
-}
-  constructor(private http:HttpClient) { }
+  user="";
+  model = {
+    username:"",
+    fullName: "",
+    phoneNumber1: 0,
+    phoneNumber2: 0,
+    address: "",
+    email: "",
+    overview: ""
+  };
+  constructor(private http:HttpClient , private router:Router) { }
 
   ngOnInit() {
-      this.http.get('/api/currentUser',{}).subscribe(res => {
-        console.log("resss",res);
-      //  console.log("resss msssg",res.msg);
-        if(res['msg']){
-          this.username = res['msg'];
-
-        }
-      }, err => {
-console.log(err.error);
-      });
+    this.http.get('/api/currentUser',{}).subscribe(res => {
+      console.log("resss",res);
+      if(res['msg']){
+        this.user = res['msg'].username;
+        this.model.username = res['msg'].username;
+        this.model.fullName = res['msg'].fullName;
+        this.model.phoneNumber1 = res['msg'].phoneNumber1;
+        this.model.phoneNumber2 = res['msg'].phoneNumber2;
+        this.model.address = res['msg'].address;
+        this.model.email = res['msg'].email;
+        this.model.overview = res['msg'].overview;
+      }
+    }, err => {
+      console.log(err.error);
+    })
   }
-
+  profile() {
+    this.model.username=this.user;
+    this.http.post('/api/profile', this.model)
+      .subscribe(res => {
+        //this.router.navigate(['/login']);
+      }, (err) => {
+          console.log(err);
+      }
+    );
+  }
 }
