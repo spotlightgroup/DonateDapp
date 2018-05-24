fada42e862915a0b9a6c213ac04bb68fcc86638f
samersalmeh <ssalmeh@gmail.com>
2018-05-25 02:31:16 +0300

    test

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from '../util/data.service';
 import { Router } from '@angular/router';
+import { MatDialog , MatDialogRef , MAT_DIALOG_DATA } from '@angular/material';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-navbar',
@@ -12,7 +14,7 @@ export class NavbarComponent implements OnInit {
   User = "";
   message = "";
   isLogged = false;
-  constructor(private http:HttpClient, private data: DataService, private router: Router) { }
+  constructor(private http:HttpClient, private data: DataService, private router: Router, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.data.isLogged = true;
@@ -38,6 +40,16 @@ export class NavbarComponent implements OnInit {
     }, err => {
       this.message = err.error.msg;
     });
-    };
 
+    };
+    openDialog(): void {
+      let dialogRef = this.dialog.open(LoginComponent, {
+        panelClass: 'custom-dialog-container'
+
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+      });
+    };
 }
