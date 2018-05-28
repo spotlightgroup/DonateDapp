9357119ef247ca77f4903e027f23120ccacaaa19
abdulhameed89 <abd.cs@hotmail.com>
2018-05-28 05:00:15 +0300

    update the ipc path to store the blocks on the terminal

--- startrinkeby-mac.sh
+++ startrinkeby-mac.sh
@@ -1,1 +1,1 @@
-atom --rinkeby --rpc --rpcapi="personal,eth,network,web3,net" --ipcpath"~/Library/Ethereum/geth.ipc"
+geth --rinkeby --rpc --rpcapi="personal,eth,network,web3,net" --ipcpath "/ethereum/rinkeby/geth.ipc"
