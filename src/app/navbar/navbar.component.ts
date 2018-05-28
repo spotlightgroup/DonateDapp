0d67cfb9113418d9f968f95e37a33f02949ee870
samersalmeh <ssalmeh@gmail.com>
2018-05-28 16:46:05 +0300

    add some animate to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,13 +4,14 @@ import { DataService } from '../util/data.service';
 import { Router } from '@angular/router';
 import { MatDialog , MatDialogRef , MAT_DIALOG_DATA } from '@angular/material';
 import { LoginComponent } from '../login/login.component';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  fadeShow=true;
   User = "";
   message = "";
   isLogged = false;
@@ -25,11 +26,13 @@ export class NavbarComponent implements OnInit {
         this.isLogged = false;
       }
     }, 1000)
-
+$(".fadeIn").hide();
+$(".circle").css('background-image', 'url(model.image)');
   }
 
 
   logout(){
+    fadeClick()
     setTimeout(() => {
       this.router.navigate(['home']);
       window.location.reload()
@@ -56,5 +59,24 @@ export class NavbarComponent implements OnInit {
         console.log('The dialog was closed');
       });
     };
-
+    fadeClick(){
+      if(this.fadeShow===true){
+        $(".fadeIn").addClass('animated fadeInRight');
+        $(".circle").css('-webkit-animation', 'spinR 600ms linear ');
+         $(".fadeIn").show();
+        setTimeout(()=> {
+          $(".fadeIn").removeClass('animated fadeInRight');
+          this.fadeShow=false;
+        }, 600)
+      }
+      if(this.fadeShow===false){
+        $(".fadeIn").addClass('animated fadeOutRight');
+        $(".circle").css('-webkit-animation', 'spinL 600ms linear ');
+        setTimeout(()=> {
+           $(".fadeIn").hide();
+          $(".fadeIn").removeClass('animated fadeOutRight');
+          this.fadeShow=true;
+        }, 600)
+      }
+    }
 }
